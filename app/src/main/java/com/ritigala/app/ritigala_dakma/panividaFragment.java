6f401db8698315d43189a89fad900a7bef723bed
thamiduNaveen <thamidunaveen@gmail.com>
2018-08-22 00:08:43 +0530

    search done

--- app/src/main/java/com/ritigala/app/ritigala_dakma/panividaFragment.java
+++ app/src/main/java/com/ritigala/app/ritigala_dakma/panividaFragment.java
@@ -4,15 +4,21 @@ package com.ritigala.app.ritigala_dakma;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class panividaFragment extends Fragment {
 
@@ -23,6 +29,7 @@ public class panividaFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_panivida,container,false);
     }
 
@@ -56,5 +63,48 @@ public class panividaFragment extends Fragment {
         };
 
         panividaRecyclerView.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    private void firebaseSearch(String searchText){
+        Query firebaseSearchQuery = dRef.orderByChild("title").startAt(searchText).endAt(searchText+"\uf8ff");
+
+        FirebaseRecyclerAdapter<Model,ViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Model, ViewHolder>(
+                Model.class,
+                R.layout.one_panivida,
+                ViewHolder.class,
+                firebaseSearchQuery
+        ) {
+            @Override
+            protected void populateViewHolder(ViewHolder viewHolder, Model model, int position) {
+                viewHolder.setDetail(getActivity(),model.title,model.getImage());
+            }
+        };
+
+        panividaRecyclerView.setAdapter(firebaseRecyclerAdapter);
+
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        getActivity().getMenuInflater().inflate(R.menu.menu_search,menu);
+        MenuItem item = menu.findItem(R.id.search_panivida);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                firebaseSearch(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                firebaseSearch(newText);
+                return false;
+            }
+        });
+
+        super.onCreateOptionsMenu(menu, inflater);
     }
 }
--- app/src/main/java/com/ritigala/app/ritigala_dakma/startActivity.java
+++ app/src/main/java/com/ritigala/app/ritigala_dakma/startActivity.java
@@ -15,7 +15,7 @@ public class startActivity extends AppCompatActivity {
       @Override
       public void run() {
         try {
-          sleep(5000);
+          sleep(2000);
         }
         catch (Exception e){
           e.printStackTrace();
